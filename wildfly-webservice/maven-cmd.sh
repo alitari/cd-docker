c4f4dac724feb16f30c7b1ca81e99bc308e94c3a
alitari <alexander.krieg@arcor.de>
2015-12-26 11:47:32 +0100

    refactor infrastructure

--- wildfly-webservice/maven-cmd.sh
+++ wildfly-webservice/maven-cmd.sh
@@ -1,4 +1,4 @@
-docker run --net host -it --rm --name mvn-build -v "$PWD":/usr/src/mymaven -v ~/.m2:/root/.m2 -v ~/.docker:/root/.docker -w /usr/src/mymaven maven:latest mvn clean install -Dbuild.number=110
+docker run --net host -it --rm --name mvn-build -v "$PWD":/usr/src/mymaven -v ~/.m2:/root/.m2 -v ~/.docker:/root/.docker -w /usr/src/mymaven maven:latest mvn clean install -Dbuild.number=110 -DqaDockerHostIp= 
 
 docker run  -it --rm --name mvn-build -v "$PWD":/usr/src/mymaven -v ~/.m2:/root/.m2  -w /usr/src/mymaven maven:latest mvn gatling:execute -f gatling_pom.xml 
 
