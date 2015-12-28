034f872eb7e71768969df8c75726ac4a048e18b8
alitari <alexander.krieg@arcor.de>
2015-12-28 21:21:02 +0100

    added jenkins

--- infrastructure/deploy.sh
+++ infrastructure/deploy.sh
@@ -1,3 +1,3 @@
 #docker run --net host -it --rm --name mvn-build -v "$PWD":/usr/src/mymaven -v $MAVEN_REPO:/root/.m2 -w /usr/src/mymaven maven:latest "mvn clean deploy -DbuildNumber=1 -DqaDockerHostIp=$DOCKER_HOST_IP_QA -DqaDockerCertPath=$DOCKER_CERT_PATH_QA DaccDockerHostIp=$DOCKER_HOST_IP_ACC -DaccDockerCertPath=$DOCKER_CERT_PATH_ACC -DinfraDockerHostIp=$DOCKER_HOST_IP_INFRA"
 
-mvn clean deploy -DbuildNumber=1 -DqaDockerHostIp=$DOCKER_HOST_IP_QA -DqaDockerCertPath=$DOCKER_CERT_PATH_QA DaccDockerHostIp=$DOCKER_HOST_IP_ACC -DaccDockerCertPath=$DOCKER_CERT_PATH_ACC -DinfraDockerHostIp=$DOCKER_HOST_IP_INFRA"
+mvn clean deploy -DbuildNumber=1 
--- infrastructure/setupInfra.sh
+++ infrastructure/setupInfra.sh
@@ -4,11 +4,13 @@ echo $DOCKER_HOST
 
 docker-machine ssh infra "sudo mkdir /etc/docker-registry; sudo chmod a+w /etc/docker-registry"
 docker-machine ssh infra "sudo mkdir /etc/nexus-data; sudo chmod a+w /etc/nexus-data"
-
+docker-machine ssh infra "sudo mkdir /etc/docker-registry-ui; sudo chmod a+w /etc/docker-registry-ui"
+docker-machine ssh infra "sudo mkdir /etc/jenkins_home; sudo chmod a+w /etc/jenkins_home"
+docker-machine ssh infra "sudo mkdir /etc/jenkins_m2; sudo chmod a+w /etc/jenkins_m2"
 
 docker-compose up -d
 docker-compose ps
 echo -e "------------------------------------------------------------\n\n"
 
-echo -e "--------------- Environment variables ----------------------\n\n"
+echo -e "--------------- Environment variables ----------------------\n"
 env | grep DOCKER
