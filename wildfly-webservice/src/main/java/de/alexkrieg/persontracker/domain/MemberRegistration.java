1820cbf46481edc1efa81e311340b2cd2e51c245
alitari <alexander.krieg@arcor.de>
2016-01-23 19:56:16 +0100

    sonar issues

--- wildfly-webservice/src/main/java/de/alexkrieg/persontracker/domain/MemberRegistration.java
+++ wildfly-webservice/src/main/java/de/alexkrieg/persontracker/domain/MemberRegistration.java
@@ -1,24 +1,8 @@
 package de.alexkrieg.persontracker.domain;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.logging.Logger;
-
-/*
- * JBoss, Home of Professional Open Source
- * Copyright 2013, Red Hat, Inc. and/or its affiliates, and individual
- * contributors by the @authors tag. See the copyright.txt in the
- * distribution for a full listing of individual contributors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
@@ -39,36 +23,33 @@ public class MemberRegistration {
     @Inject
     private Logger log;
 
-    @PersistenceContext(unitName="primary")
+    @PersistenceContext(unitName = "primary")
     private EntityManager em;
 
     @Transactional
-    public Member register(Member member) throws Exception {
+    public Member register(Member member) {
         log.info("Registering " + member.getEmail());
         em.persist(member);
         em.flush();
         return member;
     }
 
-    Member findById(long id) throws Exception {
+    Member findById(long id) {
         Session session = (Session) em.getDelegate();
         return (Member) session.byId(Member.class).load(id);
     }
 
     @Transactional
-    public void unregister(long id) throws Exception {
+    public void unregister(long id) {
         log.info("unregister member with id=" + id);
         em.createQuery("delete from Member where id = :id").setParameter("id", id).executeUpdate();
     }
 
-    public Optional<Member> findByEmail(String email) throws Exception {
+    public Optional<Member> findByEmail(String email) {
         log.info("find member with email=" + email);
-        try {
-            return Optional.of(em.createQuery("from Member where email = :email", Member.class)
-                    .setParameter("email", email).getSingleResult());
-        } catch (NoResultException e) {
-            return Optional.empty();
-        }
+        List<Member> resultList = em.createQuery("from Member where email = :email", Member.class)
+                .setParameter("email", email).getResultList();
+        return resultList.isEmpty() ? Optional.empty() : Optional.of(resultList.iterator().next());
     }
 
 }
